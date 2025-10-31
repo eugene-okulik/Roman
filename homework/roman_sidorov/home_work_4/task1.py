d97b638a50b8e8c4600b5a8e9e4f5428db3c5d0a
sidrom986-ctrl <sidrom986@gmail.com>
2025-10-31 11:44:32 +0300

    updated

--- homework/roman_sidorov/home_work_4/task1.py
+++ homework/roman_sidorov/home_work_4/task1.py
@@ -1,22 +1,22 @@
-#task1
+# task1
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
 
-#task2
+# task2
 result1 = 'результат операции: 42'
 result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
 
-result1 = int(result1[result1.index(':')+1: ])
-result2 = int(result2[result2.index(':')+1: ])
-result3 = int(result3[result3.index(':')+1: ])
+result1 = int(result1[result1.index(':') + 1:])
+result2 = int(result2[result2.index(':') + 1:])
+result3 = int(result3[result3.index(':') + 1:])
 
-print(result1+10)
-print(result2+10)
-print(result3+10)
+print(result1 + 10)
+print(result2 + 10)
+print(result3 + 10)
 
-#task3
+# task3
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-print('Students', ', '.join(students)+' study these subjects:', ', '.join(subjects))
+print('Students', ', '.join(students) + ' study these subjects:', ', '.join(subjects))
