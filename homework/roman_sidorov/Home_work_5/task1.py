1c685cb9bf0e6860e8a4902582be8fc10e5d6e83
sidrom986-ctrl <sidrom986@gmail.com>
2025-11-05 10:26:50 +0300

    sidorovR home work 5

--- homework/roman_sidorov/Home_work_5/task1.py
+++ homework/roman_sidorov/Home_work_5/task1.py
@@ -1,4 +1,9 @@
-texts = ('Etiam', 'tincidunt,', 'neque', 'erat,', 'quis', 'molestie', 'enim', 'imperdiet', 'vel.', 'Integer', 'urna',
-         'nisl,', 'facilisis', 'vitae', 'semper', 'at,', 'dignissim', 'vitae', 'libero')
-for text in texts:
-    print(text + 'ing')
+texts = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+new_texts=texts.split()
+print(new_texts)
+for text in new_texts:
+    if text[-1] in '.,':
+        new_text = text[:-1] + 'ing' + text[-1]
+    else:
+        new_text = text + 'ing'
+    print(new_text, end = ' ')
