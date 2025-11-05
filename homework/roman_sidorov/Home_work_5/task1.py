71de3af57c00d3b074a71cd6b3ca7a53c81ffb90
sidrom986-ctrl <sidrom986@gmail.com>
2025-11-05 10:25:40 +0300

    sidorovR home work 5

---
+++ homework/roman_sidorov/Home_work_5/task1.py
@@ -0,0 +1,4 @@
+texts = ('Etiam', 'tincidunt,', 'neque', 'erat,', 'quis', 'molestie', 'enim', 'imperdiet', 'vel.', 'Integer', 'urna',
+         'nisl,', 'facilisis', 'vitae', 'semper', 'at,', 'dignissim', 'vitae', 'libero')
+for text in texts:
+    print(text + 'ing')
