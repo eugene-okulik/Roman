f427672f8076e64a29924f19a777ab58f99d9f16
sidrom986-ctrl <sidrom986@gmail.com>
2025-11-05 11:08:02 +0300

    Updated home work 5 sidorovr

--- homework/roman_sidorov/Home_work_5/task1.py
+++ homework/roman_sidorov/Home_work_5/task1.py
@@ -1,9 +1,11 @@
-texts = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
-new_texts=texts.split()
-print(new_texts)
+texts = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at,'
+         ' dignissim vitae libero')
+
+new_texts = texts.split()
+
 for text in new_texts:
     if text[-1] in '.,':
         new_text = text[:-1] + 'ing' + text[-1]
     else:
         new_text = text + 'ing'
-    print(new_text, end = ' ')
+    print(new_text, end=' ')
--- homework/roman_sidorov/Home_work_5/task2.py
+++ homework/roman_sidorov/Home_work_5/task2.py
@@ -7,4 +7,5 @@ for number in numbers:
         print('Fuzz')
     elif number % 5 == 0:
         print('Buzz')
-    else: print(number)
+    else:
+        print(number)
