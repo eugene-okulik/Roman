0c83d1be00f9e659f7a6a9d8c31ff0dd1e8c27fb
sidrom986-ctrl <sidrom986@gmail.com>
2025-11-06 10:18:50 +0300

    updated lint

--- homework/roman_sidorov/home_work_6/task3.py
+++ homework/roman_sidorov/home_work_6/task3.py
@@ -3,10 +3,12 @@ result1 = 'результат операции: 54'
 result2 = 'результат работы программы: 209'
 result3 = 'результат: 2'
 
+
 def calc(result):
     text_res = result.split()[-1]
     num_res = int(text_res)
     print(num_res + 10)
+
 
 all_result = [result0, result1, result2, result3]
 
