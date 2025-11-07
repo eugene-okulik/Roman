9cff0acc628c6b08e28f1e780c054f8b4bb461c2
sidrom986-ctrl <sidrom986@gmail.com>
2025-11-07 16:42:40 +0300

    updated

--- homework/roman_sidorov/home_work_7/Task1.py
+++ homework/roman_sidorov/home_work_7/Task1.py
@@ -1,11 +1,11 @@
 import random
+
 while True:
-    salary = int(input('price:'))
+    salary = int(input('price: '))
     bonus = [True, False]
     random_bonus = bool(random.choice(bonus))
-    if random_bonus == True:
-        happy = int(salary - salary * 0.3)
-        print(salary, random_bonus, happy)
+    if random_bonus is True:
+        final_salary = int(salary - salary * 0.3)
     else:
-        unhappy = salary
-        print(salary, random_bonus, unhappy)
+        final_salary = salary
+    print(salary, random_bonus, final_salary)
--- homework/roman_sidorov/home_work_7/Task2.py
+++ homework/roman_sidorov/home_work_7/Task2.py
@@ -1,4 +1,4 @@
-def fibonacci(limit = 1000000):
+def fibonacci(limit=1000000):
     argument1, argument2 = 0, 1
     count = 0
     while count < limit:
@@ -9,13 +9,13 @@ def fibonacci(limit = 1000000):
 
 count = 1
 for number in fibonacci(1000000):
-        if count == 5:
-            print(f"5-е число:{number}")
-        elif count == 200:
-            print(f"200-е число:{number}")
-        elif count == 1000:
-            print(f"1000-е число:{number}")
-        elif count == 1000000:
-            print(f"100000-е число:{number}")
-            break
-        count += 1
+    if count == 5:
+        print(f"5-е число:{number}")
+    elif count == 200:
+        print(f"200-е число:{number}")
+    elif count == 1000:
+        print(f"1000-е число:{number}")
+    elif count == 1000000:
+        print(f"100000-е число:{number}")
+        break
+    count += 1
